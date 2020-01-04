50e4b973eb2a3d33eea838e9ea4acddf54530249
Ambarish Sridharanarayanan <5531915+deepestblue@users.noreply.github.com>
2020-01-03 23:29:29 -0800

    More refactoring. First stub of latn_to_tamil conversion.

--- assets/js/script_changer.js
+++ assets/js/script_changer.js
@@ -1,59 +1,59 @@
-let Tamil = {
-    consonants: new Set([
-        'க','ச','ட','ற','த','ப',
-        'ங','ஞ','ண','ன','ந','ம',
-        'ய','ர','ல','வ','ழ','ள',
-    ]),
-    vowels: new Set([
-        'அ','ஆ','இ','ஈ','உ','ஊ',
-        'எ','ஏ','ஐ','ஒ','ஓ','ஔ',
-    ]),
-    vowel_marks: new Set([
-        'ா','ி','ீ','ு','ூ',
-        'ெ','ே','ை','ொ','ோ','ௌ',
-        '்',
-    ]),
-    modifiers: new Set([
-        'ஃ',
-    ]),
-    misc: new Set([
-        'ௐ', '௸', '௹', '𑿩',
-    ]),
-    char_map: {
-        'க':'k','ச':'c','ட':'ṭ','ற':'ṯ','த':'t','ப':'p',
-        'ங':'ṅ','ஞ':'ñ','ண':'ṇ','ன':'ṉ','ந':'n','ம':'m',
-        'ய':'y','ர':'r','ல':'ḻ','வ':'v','ழ':'ṛ','ள':'ḷ',
-        'அ':'a','ஆ':'ā','இ':'i','ஈ':'ī','உ':'u','ஊ':'ū',
-        'எ':'e','ஏ':'ē','ஐ':'ai','ஒ':'o','ஓ':'ō','ஔ':'au',
-        'ா':'ā','ி':'i','ீ':'ī','ு':'u','ூ':'ū',
-        'ெ':'e','ே':'ē','ை':'ai','ொ':'o','ோ':'ō','ௌ':'au',
-        '்':'',
-        'ஃ':'ḵ',
-        'ௐ':'Ω', '௸':'〃','௹':'₨','𑿩':'#',
-    },
-};
+function taml_to_latn(source_text) {
+    let data = {
+        consonants: new Set([
+            'க','ச','ட','ற','த','ப',
+            'ங','ஞ','ண','ன','ந','ம',
+            'ய','ர','ல','வ','ழ','ள',
+        ]),
+        vowels: new Set([
+            'அ','ஆ','இ','ஈ','உ','ஊ',
+            'எ','ஏ','ஐ','ஒ','ஓ','ஔ',
+        ]),
+        vowel_marks: new Set([
+            'ா','ி','ீ','ு','ூ',
+            'ெ','ே','ை','ொ','ோ','ௌ',
+            '்',
+        ]),
+        modifiers: new Set([
+            'ஃ',
+        ]),
+        misc: new Set([
+            'ௐ', '௸', '௹', '𑿩',
+        ]),
+        char_map: {
+            'க':'k','ச':'c','ட':'ṭ','ற':'ṯ','த':'t','ப':'p',
+            'ங':'ṅ','ஞ':'ñ','ண':'ṇ','ன':'ṉ','ந':'n','ம':'m',
+            'ய':'y','ர':'r','ல':'ḻ','வ':'v','ழ':'ṛ','ள':'ḷ',
+            'அ':'a','ஆ':'ā','இ':'i','ஈ':'ī','உ':'u','ஊ':'ū',
+            'எ':'e','ஏ':'ē','ஐ':'ai','ஒ':'o','ஓ':'ō','ஔ':'au',
+            'ா':'ā','ி':'i','ீ':'ī','ு':'u','ூ':'ū',
+            'ெ':'e','ே':'ē','ை':'ai','ொ':'o','ோ':'ō','ௌ':'au',
+            '்':'',
+            'ஃ':'ḵ',
+            'ௐ':'Ω', '௸':'〃','௹':'₨','𑿩':'#',
+        },
+    };
 
-function tamil_str_to_ISO15919(source_text) {
     let is_consonant = false;
     let is_vowel_a = false;
     let transliterated_text = "";
     [...source_text].forEach(c => {
-        let is_implicit_a = is_consonant && ! Tamil.vowel_marks.has(c);
+        let is_implicit_a = is_consonant && ! data.vowel_marks.has(c);
         if (is_implicit_a) {
             transliterated_text += 'a';
         }
-        if (c in Tamil.char_map) {
+        if (c in data.char_map) {
             if (is_vowel_a || is_implicit_a) {
-                if (['i','u'].indexOf(Tamil.char_map[c]) >= 0) {
+                if (['i','u'].indexOf(data.char_map[c]) >= 0) {
                     transliterated_text += ':';
                 }
             }
         }
 
-        is_vowel_a = (c in Tamil.char_map) && (Tamil.char_map[c] == 'a');
-        is_consonant = Tamil.consonants.has(c);
+        is_vowel_a = (c in data.char_map) && (data.char_map[c] == 'a');
+        is_consonant = data.consonants.has(c);
 
-        transliterated_text += (c in Tamil.char_map) ? Tamil.char_map[c] : c;
+        transliterated_text += (c in data.char_map) ? data.char_map[c] : c;
     });
 
     if (is_consonant) {
@@ -61,4 +61,24 @@ function tamil_str_to_ISO15919(source_text) {
     }
 
     return transliterated_text;
+}
+
+function latn_to_taml(source_text) {
+    let data = {
+        vowels: {
+            'a':'அ','ā':'ஆ','i':'இ','ī':'ஈ','u':'உ','ū':'ஊ',
+            'e':'எ','ē':'ஏ','ai':'ஐ','o':'ஒ','ō':'ஓ','au':'ஔ',
+        },
+    };
+    while (1) {
+        let regex = /ai|au|i|u|a/g
+        let old_str = source_text;
+        source_text = source_text.replace(regex, function(match) {
+            return data.vowels[match];
+        });
+        if (source_text == old_str)
+            break;
+    }
+
+    return source_text;
 }
