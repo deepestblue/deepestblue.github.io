4ed9577d8379e11712156735fc69bb8f4c759029
Ambarish Sridharanarayanan <5531915+deepestblue@users.noreply.github.com>
2021-02-18 10:00:06 -0800

    Adjust lang tag when transliterated

--- assets/js/transform.js
+++ assets/js/transform.js
@@ -1,23 +1,26 @@
 /* global Node:false */
 
-// Transliterate all the text in the given langCode.
-function transform(langCode, transliterate) {
-    return function walk(node, langMatched) {
+// Transliterate all the text in langCode.
+function transform(docRoot, transliterate, langCode, srcScript, tgtScript) {
+    (function walk(node, langMatched) {
         switch (node.nodeType) {
             case Node.ELEMENT_NODE:
                 if (node.lang) {
-                    langMatched = node.lang == langCode;
+                    langMatched = node.lang == `${langCode}-${srcScript}`;
+                    if (langMatched) {
+                        node.lang = `${langCode}-${tgtScript}`;
+                    }
                 }
                 node.childNodes.forEach(
                     child => walk(child, langMatched));
                 break;
             case Node.TEXT_NODE:
                 if (langMatched) {
-                    node.textContent = transliterate(node.textContent);
+                    node.textContent = transliterate(srcScript, tgtScript, node.textContent);
                 }
                 break;
         }
-    };
+    })(docRoot);
 }
 
 export { transform };
